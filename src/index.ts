698196c45b15585a1cd47eda3cf31e0c49c4fe01
Wei Wen Goh <goweiwen@gmail.com>
2026-07-15 02:55:51 +0800

    chore: bump version to 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const program = new Command();
 program
   .name("immich")
   .description("Query an Immich photo library")
-  .version("0.1.0")
+  .version("0.2.0")
   .helpCommand(false);
 
 interface SearchFilters {
